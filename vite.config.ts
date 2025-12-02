59fc3613c630dd5239ced5938df898cc3da1ddbc
Dan Habot <dan.habot@gmail.com>
2025-12-01 22:54:06 -0500

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/habit-tracker-with-c/' : '/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
